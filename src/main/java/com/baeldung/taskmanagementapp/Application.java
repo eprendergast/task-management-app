30679cac2980eec7954ac9bde20978c7f9634e89
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2020-04-13 16:16:07 +0100

    Add empty beans to test init and destroy lifecycle hooks

--- src/main/java/com/baeldung/taskmanagementapp/Application.java
+++ src/main/java/com/baeldung/taskmanagementapp/Application.java
@@ -2,12 +2,15 @@ package com.baeldung.taskmanagementapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.baeldung.taskmanagementapp.config.AppConfig;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(new Class[]{LsApp.class, Config.class}, args);
+		ConfigurableApplicationContext context = SpringApplication.run(new Class[]{Application.class, AppConfig.class}, args);
 		context.close();
 	}
 
